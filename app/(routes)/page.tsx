fce635fb4d696fab72516ee3b3d9efb67afd8497
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2023-11-06 10:52:51 +0700

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,29 @@
+// import getBillboard from "@/actions/get-billboard";
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+// import getProducts from "@/actions/get-products";
+// import ProductList from "@/components/product-list";
+// import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  // const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("62d0ea68-1a49-4552-9660-c5a6f0db3572");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        alala
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          {/* <ProductList title="Featured Products" items={products} /> */}
+          lala
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
